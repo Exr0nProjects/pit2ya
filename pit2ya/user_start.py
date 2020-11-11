acae038550f30016f3d1a8b48f8d9bbcb14ae289
Exr0n <mail@exr0n.com>
2020-11-11 11:18:29 -0800

    NW priority queue

--- pit2ya/user_start.py
+++ pit2ya/user_start.py
@@ -1,54 +1,20 @@
-# def get_data():
-#     from os import getenv, path
-#     from sys import argv
-#
-#     filepath=None
-#     # if len(argv) > 1:
-#     #     filepath = argv[1]
-#     # else:
-#     filepath = getenv('XDG_DATA_HOME') + '/pit2ya/timers.csv'
-#
-#     timers = {}
-#     if not path.isfile(filepath):
-#         from csv import writer as csv_writer
-#         from toggl.api import TimeEntry
-#         from pendulum import now
-#         from os import mkdir
-#         print('config file not found... loading past month of toggl data')
-#         entries = TimeEntry.objects.all_from_reports(start=now().subtract(months=1), stop=now())
-#         print('    processing time entries...')
-#         mkdir(filepath[:filepath.rfind('/')])
-#         with open(filepath, 'w+', newline='') as wf:
-#             writer = csv_writer(wf)
-#             seen = set()
-#             for i,e in enumerate(entries):
-#                 if e.description not in seen:
-#                     seen.add(e.description)
-#                     timers[e.description] = { 'pid': int(e.pid) }
-#                     writer.writerow((e.description, e.pid or -1))
-#                 if i % 10 == 0:
-#                     print(f'processed {i} entries', end='\r')
-#     else:
-#         # when csv.reader is the fastest
-#         with open(filepath, 'r', newline='') as rf:
-#             from csv import reader
-#             rd = reader(rf)
-#             for row in rd:
-#                 timers[row[0]] = { 'pid': int(row[1]) }
-#
-#     return timers
+from dataclasses import dataclass
+
+@dataclass(order=True)
+class PrioritizedTimer:
+    from dataclasses import field
+    priority: int
+    description: str=field(compare=False)
 
 def get_data():
     from pickle import dump, load
     from os import getenv, path
-    filepath = getenv('PIT2YA_DIRPATH') or getenv('XDG_DATA_HOME') + '/pit2ya/timers.csv'
-    data = None
+    filepath = getenv('PIT2YA_DIRPATH') or getenv('XDG_DATA_HOME') + '/pit2ya/timers.pykle'
+    timers = {}
     if path.isfile(filepath):
         with open(filepath, 'rb') as rf:
-            data = load(rf)
-            print(data)
+            timers = load(rf)
     else:
-        timers = {}
         from csv import writer as csv_writer
         from toggl.api import TimeEntry
         from pendulum import now
@@ -105,5 +71,5 @@ def user_modify():
         cur.save()
 
 if __name__ == '__main__':
-    get_data()
+    user_start()
 
